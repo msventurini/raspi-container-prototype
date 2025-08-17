ccb6a8035a4bf437fcf78b63e532e77a8268a65b
Matheus Silveira Venturini <42521920+msventurini@users.noreply.github.com>
2025-08-17 17:51:12 -0300

    Changed permissions.

--- setup.sh
+++ setup.sh
@@ -14,6 +14,8 @@ cd images
 wget -nc https://downloads.raspberrypi.org/raspios_arm64/images/raspios_arm64-2023-05-03/2023-05-03-raspios-bullseye-arm64.img.xz
 # fi
 
+xz -dk 2023-05-03-raspios-bullseye-arm64.img.xz
+
 fdisk -l ./2023-05-03-raspios-bullseye-arm64.img
 echo "a"
 
