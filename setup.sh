0b1636fabbd9f3307003ff956a3835cb467bf5f7
Matheus Silveira Venturini <42521920+msventurini@users.noreply.github.com>
2025-08-17 20:30:23 -0300

    Trying to catch ssl output.

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 sudo apt-get install -y qemu-system-aarch64
 
@@ -41,29 +41,16 @@ echo "e"
 
 cp /mnt/image/kernel8.img .
 echo "f"
-
-#  fullname
-
-
 #!/bin/bash
 echo "Enter your desired login: "
 read loginData
-# if [ "$login" == "" ]; then
-#     $login = "pi"
-# fi
+
 echo "$loginData"
 result=$(openssl passwd -6)
 
-
-
-# senha gerada
-# $6$DLJ421NCDPLOdSpN$6o797./rv6UVfR3PmAxbkElkVILTSlBn4JINIJbfF4W.oNM4XvTLOZA7KVw1MHH0p3icTixDq0xNeZRHKHyjJ.
-
 echo "${loginData}:${result}" | sudo tee /mnt/image/userconf
 
-# sudo touch /mnt/image/ssh
+sudo touch /mnt/image/ssh
 
-# qemu-img resize ./2023-05-03-raspios-bullseye-arm64.img 8G
-
-# qemu-system-aarch64 -machine raspi3b -cpu cortex-a72 -nographic -dtb ./bcm2710-rpi-3-b-plus.dtb -m 1G -smp 4 -kernel ./kernel8.img -sd ./images/2023-05-03-raspios-bullseye-arm64.img -append "rw earlyprintk loglevel=8 console=ttyAMA0,115200 dwc_otg.lpm_enable=0 root=/dev/mmcblk0p2 rootdelay=1" -device usb-net,netdev=net0 -netdev user,id=net0,hostfwd=tcp::2222-:22
+qemu-system-aarch64 -machine raspi3b -cpu cortex-a72 -nographic -dtb ./bcm2710-rpi-3-b-plus.dtb -m 1G -smp 4 -kernel ./kernel8.img -sd ./images/2023-05-03-raspios-bullseye-arm64.img -append "rw earlyprintk loglevel=8 console=ttyAMA0,115200 dwc_otg.lpm_enable=0 root=/dev/mmcblk0p2 rootdelay=1" -device usb-net,netdev=net0 -netdev user,id=net0,hostfwd=tcp::2222-:22
 
