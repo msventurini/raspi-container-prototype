b812b3ac955424ff7eb3c37027d4f45e074bc818
Matheus Silveira Venturini <42521920+msventurini@users.noreply.github.com>
2025-08-17 19:25:28 -0300

    Changed permissions.

--- setup.sh
+++ setup.sh
@@ -55,5 +55,4 @@ echo "f"
 
 # qemu-img resize ./2023-05-03-raspios-bullseye-arm64.img 8G
 
-qemu-system-aarch64 -machine raspi3b -cpu cortex-a72 -nographic -dtb ./bcm2710-rpi-3-b-plus.dtb -m 1G -smp 4 -kernel ./kernel8.img -sd ./images/2023-05-03-raspios-bullseye-arm64.img -append "rw earlyprintk loglevel=8 console=ttyAMA0,115200 dwc_otg.lpm_enable=0 root=/
-dev/mmcblk0p2 rootdelay=1" -device usb-net,netdev=net0 -netdev user,id=net0,hostfwd=tcp::2222-:22
+qemu-system-aarch64 -machine raspi3b -cpu cortex-a72 -nographic -dtb ./bcm2710-rpi-3-b-plus.dtb -m 1G -smp 4 -kernel ./kernel8.img -sd ./images/2023-05-03-raspios-bullseye-arm64.img -append "rw earlyprintk loglevel=8 console=ttyAMA0,115200 dwc_otg.lpm_enable=0 root=/dev/mmcblk0p2 rootdelay=1" -device usb-net,netdev=net0 -netdev user,id=net0,hostfwd=tcp::2222-:22
