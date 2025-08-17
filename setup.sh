c6353a0cbe744eb9a525a6b8d3ac0cc12dd6ec63
Matheus Silveira Venturini <42521920+msventurini@users.noreply.github.com>
2025-08-17 01:08:19 -0300

    Added initial files.

--- setup.sh
+++ setup.sh
@@ -2,6 +2,12 @@
 
 apt-get install -y qemu-system-aarch64
 
+cd ..
+
+mkdir raspi-container
+
+apt-get install -y qemu-system-aarch64
+
 wget https://downloads.raspberrypi.org/raspios_arm64/images/raspios_arm64-2023-05-03/2023-05-03-raspios-bullseye-arm64.img.xz
 
 fdisk -l ./2023-05-03-raspios-bullseye-arm64.img
